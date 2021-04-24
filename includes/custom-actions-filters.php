7f278ab3c948c41598a61e3ab7c61f1e3517ddb0
buntybuddyboss <bhargav@buddyboss.com>
2021-04-24 14:31:29 +0530

    Manage Post Matadata

--- includes/custom-actions-filters.php
+++ includes/custom-actions-filters.php
@@ -1,1 +1,17 @@
 <?php
+
+function metadata_handle_callback( $post_id, $post, $update ) {
+
+	if ( $update ) {
+        return;
+    }
+
+	// add_post_meta( $post_id, 'test_meta_key', 'yes' );
+
+	// update_post_meta( $post_id, 'test_meta_key', 'no' );
+
+    delete_post_meta( $post_id, 'test_meta_key' );
+
+}
+
+add_action( 'save_post', 'metadata_handle_callback', 10, 3 );
