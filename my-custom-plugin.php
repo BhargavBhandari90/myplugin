8a57cbc39deb7b362f8920d340a2ed16dff0d200
buntybuddyboss <bhargav@buddyboss.com>
2021-07-29 00:02:56 +0530

    Add css and JS

--- my-custom-plugin.php
+++ my-custom-plugin.php
@@ -8,6 +8,9 @@ Version: 1.0.0
 Author URI: http://bhargavb.wordpress.com/
 */
 
+define( 'MY_PLUGIN_URL', plugin_dir_url(__FILE__) );
+define( 'MY_PLUGIN_VER', '1.0.0' );
+
 include plugin_dir_path( __FILE__ ) . 'includes/custom-post-type.php';
 include plugin_dir_path( __FILE__ ) . 'includes/class-custom-widget.php';
 include plugin_dir_path( __FILE__ ) . 'includes/custom-ajax.php';
