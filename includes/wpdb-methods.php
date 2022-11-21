d28309587a16f72322e9983004ad6e6944a02bb3
Bunty <bhargav@buddyboss.com>
2022-11-21 07:29:48 +0530

    Update WPDB

--- includes/wpdb-methods.php
+++ includes/wpdb-methods.php
@@ -2,26 +2,47 @@
 
 function bwp_wbdb_methods() {
 
-	$can_insert = $_GET['insert'];
-
-	if ( empty( $can_insert ) ) {
-		return;
-	}
-
 	global $wpdb;
 
 	$table = $wpdb->prefix . 'bwp';
-	$data  = array(
-		'first_name' => 'Bunty',
-		'last_name'  => 'WP',
-	);
 
-	$wpdb->insert(
-		$table,
-		$data
-	);
+	$can_insert = $_GET['insert'];
 
-	echo 'Data inserted...';
+	if ( ! empty( $can_insert ) ) {
+
+		$data  = array(
+			'first_name' => 'Bunty',
+			'last_name'  => 'WP',
+		);
+
+		$wpdb->insert(
+			$table,
+			$data
+		);
+
+		echo 'Data inserted...';
+
+	}
+
+	$can_update = $_GET['update'];
+
+	if ( ! empty( $can_update ) ) {
+
+		$wpdb->update(
+			$table,
+			array(
+				'first_name' => 'ABC',
+				'last_name'  => 'XYZ',
+			),
+			array(
+				'age'       => '20',
+				'last_name' => 'WP',
+			),
+		);
+
+		echo 'Data Updated...';
+
+	}
 
 }
 
